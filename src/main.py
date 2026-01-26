fb4222bfa9d28c5efe9e7c812b3de0c80423a453
Davi Oliveira <davicbtoliveira@hotmail.com>
2026-01-26 19:57:10 -0400

    REFACT

--- src/main.py
+++ src/main.py
@@ -1,51 +1,109 @@
-import os
+import sys
+from pathlib import Path
 import shutil
-from textnode import TextNode
-from textnode import TextType
+
 from markdown_blocks import markdown_to_html_node
+from htmlnode import HTMLNode
 
-def copy_directory(src, dst):
-    if os.path.exists(dst):
-        shutil.rmtree(dst)
-    shutil.copytree(src, dst)
 
-def extract_title(markdown):
-    for line in markdown.split('\n'):
-        if line.startswith('# '):
-            return line[2:].strip()
-    raise Exception("No H1 title found in markdown")
+def extract_title(htmlnode: HTMLNode) -> str:
+    elements = htmlnode.children
+    if not elements or len(elements) < 1:
+        raise Exception("Bad markdown")
 
-def generate_page(from_path, template_path, dest_path):
+    first_element = elements[0]
+
+    if (
+        first_element.tag != "h1"
+        or not first_element.children
+        or len(first_element.children) == 0
+    ):
+        raise Exception("Bad markdown")
+
+    if first_element.children[0].value is None:
+        raise Exception("Bad markdown")
+
+    return first_element.children[0].value
+
+
+def generate_page(
+    from_path: Path, template_path: Path, dest_path: Path, basepath: str
+) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
-    with open(from_path, 'r') as f:
-        markdown_content = f.read()
+    markdown = from_path.read_text()
+    template = template_path.read_text()
 
-    with open(template_path, 'r') as f:
-        template_content = f.read()
+    html = markdown_to_html_node(markdown)
 
-    html_content = markdown_to_html_node(markdown_content).to_html()
-    title = extract_title(markdown_content)
+    title = extract_title(html)
 
-    full_html = template_content.replace('{{ Title }}', title).replace('{{ Content }}', html_content)
+    template = (
+        template.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html.to_html())
+        .replace('href="/', f'href="{basepath}')
+        .replace('src="/', f'src="{basepath}')
+    )
 
-    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    with open(dest_path, 'w') as f:
-        f.write(full_html)
+    if not dest_path.parent.exists():
+        dest_path.parent.mkdir()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    for root, _, files in os.walk(dir_path_content):
-        for file in files:
-            if file.endswith('.md'):
-                from_path = os.path.join(root, file)
-                relative_path = os.path.relpath(from_path, dir_path_content)
-                dest_path = os.path.join(dest_dir_path, os.path.splitext(relative_path)[0] + '.html')
-                generate_page(from_path, template_path, dest_path)
+    dest_path.write_text(template)
+
+
+def copy_directory_recursive(src: Path, dst: Path) -> None:
+    if not dst.exists():
+        dst.mkdir()
+
+    for f in src.iterdir():
+        new_f = dst / f.name
+        if f.is_dir():
+            copy_directory_recursive(f, new_f)
+        else:
+            print(f"Copying {f} to {new_f}")
+            shutil.copy(f, new_f)
+
+
+def generate_pages_recursive(
+    dir_path_content: Path, template_path: Path, dest_dir_path: Path, basepath: str
+) -> None:
+    if not dest_dir_path.exists():
+        dest_dir_path.mkdir()
+
+    for f in dir_path_content.iterdir():
+        new_f = dest_dir_path / f.name
+        if f.is_dir():
+            generate_pages_recursive(f, template_path, new_f, basepath)
+        else:
+            if f.suffix.lower() == ".md":
+                generate_page(f, template_path, new_f.with_suffix(".html"), basepath)
+            else:
+                print(f"Ignoring {f}")
+
 
 def main():
-    copy_directory('static', 'public')
+    if len(sys.argv) == 2:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
 
-    generate_pages_recursive('content', 'template.html', 'public')
+    print(basepath)
+    project_root = Path(__file__).parent.parent
+
+    dest_path = project_root / "docs"
+    static_dir = project_root / "static"
+
+    # remove before copying
+    if dest_path.exists():
+        shutil.rmtree(dest_path)
+
+    copy_directory_recursive(static_dir, dest_path)
+
+    from_path = project_root / "content"
+    template_path = project_root / "template.html"
+
+    generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
 
 if __name__ == "__main__":
     main()
